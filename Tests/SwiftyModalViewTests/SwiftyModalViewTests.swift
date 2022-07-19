d7aab3b8422680c4a7ad2a5bd6061fa45766a4dc
Ole Kallerud <ole@kallerud.no>
2022-07-19 18:56:15 +0200

    Changed name to 'SwiftyModalView'

---
+++ Tests/SwiftyModalViewTests/SwiftyModalViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyModal
+
+final class SwiftyModalTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        // XCTAssertEqual(ModalView().text, "Hello, World!")
+    }
+}
